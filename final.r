aac7e08174610bd363a4295e7a94162d3d02e775
Sh1r0Yaksha <541r0yak54a@gmail.com>
2024-04-01 20:38:32 +0530

    Plots done

---
+++ final.r
@@ -0,0 +1,137 @@
+#Clearing envirnment variables
+rm(list = ls())
+
+# Installing and importing libraries
+if (rstudioapi::isAvailable()) {
+  if (require("rstudioapi") != TRUE) {
+    install.packages("rstudioapi")
+  }else {
+    library(rstudioapi)
+  }
+  wdir <- dirname(getActiveDocumentContext()$path)
+}
+library("biomaRt")
+
+# Setting Working Directory
+setwd(paste0(wdir, "/Data"))
+
+
+# Getting the start and end index of proteins
+start_index <- 1
+end_index <- 58302
+
+# Importing nef data
+nef_ratio <- read.delim("infect.txt", header = FALSE)
+
+
+# Getting the unique IDs of all proteins and saving them in a csv file
+
+get_unique_ids <- function() {
+  all_ids <- c()
+
+  for (file in nef_ratio$V1){
+    f <- read.delim(paste0(file, ".htseq"), header = FALSE)
+    f_clean <- head(f, -5)
+
+    # Extract IDs from f_clean and append to all_ids vector
+    ids_in_file <- unique(f_clean$V1)
+    all_ids <- c(all_ids, ids_in_file)
+  }
+
+  unique(all_ids)
+}
+
+proteins <- data.frame(id = get_unique_ids())
+
+for (i in seq(1, nrow(proteins))) {
+  proteins$p_value[i] <- NA
+  proteins$corr[i] <- NA
+}
+
+View(proteins)
+
+write.csv(proteins, file = "proteins.csv", row.names = FALSE)
+
+# Creating the proteins df from the previously made csv file
+proteins <- read.csv(file = "proteins.csv", na.strings = c("NA", "N/A", ""))
+View(proteins)
+
+# Function to get the Reads per Millioin(RPM) of a given protein
+get_rpm <- function(protein) {
+
+  rpm <- c()
+
+  for (file in nef_ratio$V1){
+    f <- read.delim(paste0(file, ".htseq"), header = FALSE)
+    f_clean <- head(f, -5)
+
+    RPM <- (f_clean[f_clean$V1 == protein, ]$V2 / sum(f_clean$V2)) * 1000000
+    rpm <- append(rpm, RPM)
+  }
+  rpm
+}
+
+
+# Function to calculate the correlation and p_value of a protein from the list of proteins
+# with start and end index set previously
+# This was done so that the code could be run many times with different indices
+# on many laptops as there are a total of 58,302 unique proteins
+get_correlation <- function(start, end) {
+  for (i in seq(start, end)) {
+    protein_id <- proteins$id[i]
+    protein_p_value <- proteins$p_value[i]
+    protein_corr <- proteins$corr[i]
+    if (is.na(protein_p_value) && is.na(protein_corr)) {
+      rpm_values <- get_rpm(protein_id)
+
+      # Add RPM values to nef_ratio with a new column name based on the protein ID # nolint
+      nef_ratio[[paste0("RPM_", toupper(protein_id))]] <<- rpm_values
+      corr <- cor.test(nef_ratio$V2, rpm_values, method = "pearson")
+      proteins[i, "corr"] <<- corr$estimate
+      proteins[i, "p_value"] <<- corr$p.value
+      cat("Protein", i, "out of", end, "done", "\n")
+    }
+  }
+}
+
+get_correlation(start_index, end_index)
+
+View(proteins)
+
+# Writing the updated proteins df with correlation and p_value to a csv
+write.csv(proteins, file = "proteins.csv", row.names = FALSE)
+
+# Filtering the proteins whose pearson correlation p-values gave value less than 0.05 and removing null values
+proteins_filtered <- proteins[proteins$p_value < 0.05, ]
+proteins_filtered_clean <- proteins_filtered[complete.cases(proteins_filtered), ]
+
+View(proteins_filtered_clean)
+
+# Using biomaRt library to find the gene hgnc symbol based on the ensemble id
+mart <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
+genes <-  proteins_filtered_clean$id
+
+gene_IDs <- getBM(filters= "ensembl_gene_id", attributes= c("ensembl_gene_id","hgnc_symbol"),
+              values = genes, mart= mart)
+
+proteins_filtered_clean <- merge(proteins_filtered_clean, gene_IDs, by.x = "id", by.y = "ensembl_gene_id", all.x = TRUE)
+
+
+View(proteins_filtered_clean)
+
+# Sorting the proteins in ascending order of p-value to find the best correlated proteins
+proteins_sorted <- proteins_filtered_clean[order(proteins_filtered_clean$p_value), ]
+
+View(proteins_sorted)
+
+# Writing the final dataframe to a csv
+write.csv(proteins_sorted, file = "proteins_below_threshold.csv", row.names = FALSE)
+
+# Saving the infectivity ratio with rpm in another csv file
+for (i in seq(1, nrow(proteins_sorted))) {
+  protein_id <- proteins_sorted$id[i]
+  get_rpm(protein_id)
+  cat("Protein: ", i, "out of ", nrow(proteins_sorted), "done\n")
+}
+
+write.csv(nef_ratio, file = "infectivity_with_rpm.csv", row.names = FALSE)
---
+++ plots.r
@@ -0,0 +1,157 @@
+dev.off()
+rm(list = ls())
+
+serinc5 <- "ENSG00000164300"
+
+if (rstudioapi::isAvailable()) {
+  if (require("rstudioapi") != TRUE) {
+    install.packages("rstudioapi")
+  }else {
+    library(rstudioapi)
+  }
+  wdir <- dirname(getActiveDocumentContext()$path)
+}
+library("reporter")
+
+# Setting Working Directory
+setwd(paste0(wdir, "/Data"))
+
+# Importing nef data
+nef_ratio <- read.csv("infectivity_with_rpm.csv", header = TRUE)
+
+# Plotting bar plot (Fig 1a)
+color_vector <- c("red", "red", "red", "red", "red",
+                  "red", "red", "red", "green", "green",
+                  "green", "green", "green", "blue", "blue")
+
+
+barplot(nef_ratio$infectivity_ratio,
+        horiz = TRUE,
+        col = color_vector,
+        names.arg = nef_ratio$cell_type,
+        xlab = "Nef+/Nef - infectivity ratio",
+        ylab = "Cell Type",
+        xlim = c(0, 40), las = 1, cex.axis = 0.75, cex.names = 0.6,
+        main = "Bar Plot of cell lines according to their infectivity ratio")
+
+# Plotting scatter plot of SERINC5 (Fig 1d)
+
+rpm_serinc5 <- nef_ratio[, paste0("RPM_", serinc5)]
+
+corr <- cor.test(nef_ratio$infectivity_ratio, rpm_serinc5 , method = "pearson")
+
+linear_model <- lm(rpm_serinc5 ~ nef_ratio$infectivity_ratio)
+summary_linear_model <- summary(linear_model)
+
+r <- corr$estimate
+p_value <- corr$p.value
+r_squared <- summary_linear_model$r.squared
+
+
+plot(nef_ratio$infectivity_ratio, rpm_serinc5, pch = 19, col = color_vector, xlab = "infectivity ratio", ylab = "RPM")
+abline(linear_model)
+
+text(4, max(rpm_serinc5),
+     paste("r", ":", round(r, 3)), pos = 2)
+text(5, max(rpm_serinc5 - 20),
+     paste("R" %p% supsc("2"), ":", round(r_squared, 4)), pos = 2)
+text(4.5, max(rpm_serinc5 - 40),
+     "P < 0.001", pos = 2)
+
+
+
+# Hypothesis testing
+
+proteins_below_threshold <- read.csv(file = "proteins_below_threshold.csv",
+                                     header = TRUE)
+
+neg_corr_df <- proteins_below_threshold[proteins_below_threshold$corr < 0, ]
+
+plot_correlation <- function(number, df, method) {
+  iterations <- 0
+  i <- 1
+  while (iterations < number) {
+    protein_id <- df$id[i]
+    protein_name <- df$hgnc_symbol[i]
+    rpm <- nef_ratio[, paste0("RPM_", protein_id)]
+
+    shapiro <- shapiro.test(rpm)
+
+    method_here <- ifelse(shapiro$p.value < 0.05, "pearson", "spearman")
+
+    if (method_here != method) {
+      i <- i + 1
+      next
+    }
+
+    corr <- cor.test(nef_ratio$infectivity_ratio, rpm, method = method)
+    linear_model <- lm(rpm ~ nef_ratio$infectivity_ratio)
+    # summary_linear_model <- summary(linear_model)
+
+    r <- corr$estimate
+    p_value <- corr$p.value
+
+
+    plot(nef_ratio$infectivity_ratio, rpm, pch = 19, col = color_vector,
+         xlab = "infectivity ratio", ylab = "RPM",
+         main = paste("Protein:", protein_name, "Method: ", method,
+                      "P:", round(p_value, 4),
+                      "r:", r))
+
+    abline(linear_model)
+    i <- i + 1
+    iterations <- iterations + 1
+  }
+}
+
+# Plot Positive and Negative Correlation top 5
+proteins_num <- 5
+
+num_rows <- ceiling(sqrt(proteins_num))  # Round up to the nearest integer
+num_cols <- ceiling(proteins_num / num_rows)
+
+par(mfrow = c(num_rows, num_cols))
+plot_correlation(proteins_num, proteins_below_threshold, "pearson")
+mtext("Top 5 Positive Correlation with normally distributed data",
+      side = 3, line = -1.5, outer = TRUE, cex = 1.5, font = 2)
+
+par(mfrow = c(num_rows, num_cols))
+plot_correlation(proteins_num, neg_corr_df, "pearson")
+mtext("Top 5 Negative Correlation with normally distributed data",
+      side = 3, line = -1.5, outer = TRUE, cex = 1.5, font = 2)
+
+par(mfrow = c(num_rows, num_cols))
+plot_correlation(proteins_num, proteins_below_threshold, "spearman")
+mtext("Top 5 Positive Correlation with non-normally distributed data",
+      side = 3, line = -1.5, outer = TRUE, cex = 1.5, font = 2)
+
+par(mfrow = c(num_rows, num_cols))
+plot_correlation(proteins_num, neg_corr_df, "spearman")
+mtext("Top 5 - Negative Correlation with non-normally distributed data",
+      side = 3, line = -1.5, outer = TRUE, cex = 1.5, font = 2)
+
+# Plot PCA
+columns_to_remove <- c("filename", "cell_type")
+PCA_filter_df <- nef_ratio[, !(names(nef_ratio) %in% columns_to_remove)]
+
+data_matrix <- as.matrix(PCA_filter_df)  # Convert dataframe to matrix
+
+pca_result <- prcomp(data_matrix, scale. = TRUE)
+
+# View PCA results
+summary(pca_result)  # Summary of the PCA result was applied)
+par(mfrow = c(1,1))
+plot(pca_result, type = "l", main = "Scree Plot")
+
+# Calculate the percentage of variance explained by each component
+variance_explained <- (pca_result$sdev^2) / sum(pca_result$sdev^2) * 100
+
+# Create a bar plot for the scree plot
+barplot(variance_explained,
+        names.arg = paste0("PC", seq(1, length(variance_explained))),
+        xlab = "Principal Component",
+        ylab = "Percentage of Variance Explained",
+        ylim = c(1, 100),
+        main = "Scree Plot with Percentage of Variance Explained",
+        col = "skyblue")
+axis(2, at = seq(0, 100, by = 10), labels = seq(0, 100, by = 10))
